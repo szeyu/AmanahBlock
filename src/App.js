cecc8be1c0ef6c2309c80f59b7f89feba1b77f1b
szeyu <szeyusim@gmail.com>
2025-04-20 05:57:30 +0800

    Remove duplicate import statement for React in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import theme from './styles/theme';
